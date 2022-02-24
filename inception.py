3b2c68067c75fa7cdd985bcaf5c60f28aad67371
Lucas Massa <lmm@ic.ufal.br>
2022-02-24 12:48:57 -0300

    Cleaning old code

--- inception.py
+++ inception.py
@@ -19,36 +19,6 @@ from tensorflow.keras.optimizers.schedules import ExponentialDecay
 import inception_naive
 import inception_v1
 
-def inception_module(layer_in, f1, f2, f3):
-    # 1x1 conv
-    conv1 = Conv2D(f1, (1,1), padding='same', activation='relu')(layer_in)
-    # 3x3 conv
-    conv3 = Conv2D(f2, (3,3), padding='same', activation='relu')(layer_in)
-    # 5x5 conv
-    conv5 = Conv2D(f3, (5,5), padding='same', activation='relu')(layer_in)
-    # 3x3 max pooling
-    # pool = MaxPooling2D((2,2), strides=(1,1), padding='same')(layer_in)
-    # concatenate filters, assumes filters/channels last
-    layer_out = concatenate([conv1, conv3, conv5], axis=-1)
-    return layer_out
-
-def sister_functional_generation(input_layer):
-    inception1 = inception_module(input_layer, 64, 128, 128)
-    inception2 = inception_module(inception1, 128, 128, 128)
-    # drop1 = SpatialDropout2D(0.2)(inception2)
-    pool1 = MaxPooling2D(pool_size=(2, 2))(inception2)
-    inception3 = inception_module(pool1, 64, 64, 64)
-    inception4 = inception_module(inception3, 64, 64, 64)
-    # drop2 = SpatialDropout2D(0.2)(inception4)
-    pool2 = MaxPooling2D(pool_size=(2, 2))(inception4)
-    inception5 = inception_module(pool2, 64, 64, 64)
-    inception6 = inception_module(inception5, 64, 64, 64)
-    pool3 = MaxPooling2D(pool_size=(2, 2))(inception6)
-    # inception7 = inception_module(pool3, 32, 32, 32)
-    # inception8 = inception_module(inception7, 32, 32, 32)
-    flat = Flatten()(pool3)
-    return flat
-
 def RMSE(vectors):
     (featsA, featsB) = vectors
     mean = kerasBackend.sqrt(kerasBackend.mean(kerasBackend.square(featsA - featsB), axis=1,keepdims=True))
